f741eaef8dfe8894a8cd33dc8bb0ff67641b1d7b
Joshua Coles <josh@coles.to>
2021-03-29 20:19:23 +0100

    Vault Backup: 2021-03-29 20:19:23

--- file-upload.bash
+++ file-upload.bash
@@ -1,7 +1,7 @@
 echo $$ > .fus-pid
 
 npx files-upload-server &
-ssh -R 5000:localhost:8008 -N -o ExitOnForwardFailure=yes playground &
+ssh -v -R 5000:localhost:8008 -N -o ExitOnForwardFailure=yes -p 8422 apps@joshuacoles.me &
 PID=$!
 trap 'kill $PID' 1 2 15
 
