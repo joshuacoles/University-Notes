8a06a0dab72dd3db759ef347e4c01ff397e2114d
Joshua Coles <josh@coles.to>
2021-08-09 13:52:32 +0100

    Some fixer scripts

--- x.sh
+++ x.sh
@@ -1,11 +1,11 @@
-sd '^(Definition) (\d).' '## $1 $2' *.md
-sd '^(Theorem) (\d).' '## $1 $2' *.md
-sd '^(Remark) (\d).' '## $1 $2' *.md
-sd '^(Example) (\d).' '## $1 $2' *.md
-sd '^(Proposition) (\d).' '## $1 $2' *.md
-sd '^(Corollary) (\d).' '## $1 $2' *.md
+sd '^(Definition) (\d).' '### $1 $2' *.md
+sd '^(Theorem) (\d).' '### $1 $2' *.md
+sd '^(Remark) (\d).' '### $1 $2' *.md
+sd '^(Example) (\d).' '### $1 $2' *.md
+sd '^(Proposition) (\d).' '### $1 $2' *.md
+sd '^(Corollary) (\d).' '### $1 $2' *.md
 
-sd '^Proof.' '### Proof\n' *.md
+sd '^Proof.' '#### Proof\n' *.md
 
 sd 'ℚ' '\Q' *.md
 sd 'ℤ' '\Z' *.md
---
+++ y.sh
@@ -0,0 +1,9 @@
+sd '^(Definition) (\d).' '### $1 $2' *.md
+sd '^(Theorem) (\d).' '### $1 $2' *.md
+sd '^(Remark) (\d).' '### $1 $2' *.md
+sd '^(Example) (\d).' '### $1 $2' *.md
+sd '^(Proposition) (\d).' '### $1 $2' *.md
+sd '^(Corollary) (\d).' '### $1 $2' *.md
+
+sd '^Proof.' '#### Proof\n' *.md
+
